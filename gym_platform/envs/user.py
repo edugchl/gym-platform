ebd99e137234322def5137b3ac4bf5eba70309f4
garychl <garychl320@gmail.com>
2021-03-26 18:10:36 +0800

    added world env, and measures of user states

--- gym_platform/envs/user.py
+++ gym_platform/envs/user.py
@@ -10,23 +10,47 @@ class UserEnv(gym.Env):
     job_encoder = LabelBinarizer()
     job_encoder.fit(JOB_LIST)
 
-    def __init__(self, job_type, num_dependents, notification_burden):
+    def __init__(self, job_type, num_dependents):
         super().__init__()
         self.job_type = job_type
         # Encoded the job type and remove last dim to avoid collinearity 
         self.job_encoded = job_encoder.transform(job_type)[:,:-1]
         self.num_dependents = num_dependents
-        self.initial_burden = notification_burden
+        self.state = None
+
+    def _compute_freeness(self):
+        # model free time based on job and dependents
+        # A simple way to compute to get the program up and run
+        if day_of_week is in range(1,6):
+            freeness = 0.5
+        else:
+            freeness = 0.9
+
+        dependent_discount = -np.log(self.num_dependents+30)+2
+        freeness *= dependent_discount
+        return freeness
+
+    def _compute_notification_burden(self):
+        # TODO: model notification burden
+        # A simple way to compute to get the program up and run
+        return notification_burden
     
     def _get_obs(self):
-        return np.append(self.job_encoded, self.num_dependents, self.notification_burden)
+        """Transform the state into observation. \
+        The benefit of computing freeness and notification burden is that \
+        we can encode our knowledge about the human behaviour into the function, \
+        so that our agent does not need to learn from an extremely large state space.
+        """
+        freeness = self._compute_freeness()
+        notification_burden = self._compute_notification_burden()
+        return np.array([freeness, notification_burden])
 
     def reset(self):
-        """Reset the user state to initial values."""
-        self.notification_burden = self.initial_burden
+        """Reset the user state to initial values, and return obs."""
         return self._get_obs()
 
     def step(self, action):
+        # TODO: model reward as a function of free time and notification burden
         pass
 
     def render(self, mode):
--- gym_platform/envs/world.py
+++ gym_platform/envs/world.py
@@ -1,3 +1,6 @@
+import time
+
+import numpy as np
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
@@ -6,6 +9,23 @@ from gym.utils import seeding
 class WorldEnv(gym.Env):
     def __init__(self):
         super().__init__()
+
+    def _encode_cycle(self, t, max_val):
+        # encode the datetime and capture the cyclical pattern
+        # https://www.kaggle.com/avanwyk/encoding-cyclical-features-for-deep-learning
+        sin_feat = np.sin(2 * np.pi * t/max_val)
+        cos_feat = np.cos(2 * np.pi * t/max_val)
+        return sin_feat, cos_feat
+
+    def _get_obs(self):
+        year, month, day, hour, min, day_of_week = map(int, time.strftime("%Y %m %d %H %M %w").split())
+        month_feat = self._encode_cycle(month, 12)
+        day_feat = self._encode_cycle(day, 31)
+        hour_feat = self._encode_cycle(hour, 23)
+        min_feat = self._encode_cycle(month, 59)
+        dow_feat = self._encode_cycle(day_of_week, 6)
+        datetime_state = np.array([month_feat, day_feat, hour_feat, min_feat, dow_feat]).flatten()
+        return datetime_state
 
     def reset(self):
         obs = self._get_obs()
