5a7fc9879bf5b02d66bfc08577c606c537d8e595
garychl <garychl320@gmail.com>
2021-04-22 18:21:08 +0800

    update to test a2c

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ from gym_platform import PlatformEnv, UserEnv, WorldEnv
 wandb.init(project='rl-notification-system')
 world_env = WorldEnv(ratio=3600)
 user_env = UserEnv(world_env, 'WHITE COLLAR', 0)
-platform = PlatformEnv(user_env, world_env, 30*24)
+platform = PlatformEnv(user_env, world_env, 7*24)
 
 model = A2C('MlpPolicy', platform, verbose=0)
-model.learn(total_timesteps=1000)
+model.learn(total_timesteps=7*24*100)
 
 terminal = False
 obs = platform.reset()
-while not terminal:
+while True:
     time.sleep(1)
     # action = np.random.choice(2)
     action, _state = model.predict(obs, deterministic=True)
     new_obs, r, terminal, _ = platform.step(action)
-    wandb.log({'reward':r})
+    wandb.log({'action': action, 'reward':r})
     print(f'state: {platform.user_env.state}\n, obs: {new_obs}\n, action:{action}, reward:{r} \n')
