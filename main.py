c863e3f3877d8d93a8bd062e6eb72063b6eb2d5e
garychl <garychl320@gmail.com>
2021-04-23 17:20:58 +0800

    update fatal error: wrongly create new_obs varaible

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ world_env = WorldEnv(ratio=3600)
 user_env = UserEnv(world_env, 'WHITE COLLAR', 0)
 platform = PlatformEnv(user_env, world_env, 7*24)
 
-model = A2C('MlpPolicy', platform, verbose=0)
-model.learn(total_timesteps=7*24*100)
+model = A2C('MlpPolicy', platform, verbose=1)
+model.learn(total_timesteps=7*24)
 
 terminal = False
 obs = platform.reset()
@@ -22,6 +22,6 @@ while True:
     time.sleep(1)
     # action = np.random.choice(2)
     action, _state = model.predict(obs, deterministic=True)
-    new_obs, r, terminal, _ = platform.step(action)
+    obs, r, terminal, _ = platform.step(action)
     wandb.log({'action': action, 'reward':r})
-    print(f'state: {platform.user_env.state}\n, obs: {new_obs}\n, action:{action}, reward:{r} \n')
+    print(f'state: {platform.user_env.state}\n, obs: {obs}\n, action:{action}, reward:{r} \n')
