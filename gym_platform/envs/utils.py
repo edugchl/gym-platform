f11aa9c8f7e6e3cee2b2e83b3705b76f8409ef93
garychl <garychl320@gmail.com>
2021-05-06 18:03:03 +0800

    added elapsed days calculation

--- gym_platform/envs/utils.py
+++ gym_platform/envs/utils.py
@@ -3,7 +3,10 @@ import collections
 
 def time_elapsed(t1, t2, unit='hours', digit=2):
     elapsed = t2 - t1
-    if unit == 'hours':
+    
+    if unit == 'days':
+        return round(elapsed.total_seconds()/86400, digit)
+    elif unit == 'hours':
         return round(elapsed.total_seconds()/3600, digit)
     elif unit == 'minutes':
         return round(elapsed.total_seconds()/60, digit)
