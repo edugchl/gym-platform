518f3006f92720e62bfbd774cdaba7a5193b2527
garychl <garychl320@gmail.com>
2021-05-07 11:37:09 +0800

    use clock

--- gym_platform/__init__.py
+++ gym_platform/__init__.py
@@ -1,7 +1,7 @@
 from gym.envs.registration import register, make
 from gym_platform.envs.learning_platform import LearningPlatform
 from gym_platform.envs.user import User
-from gym_platform.envs.world import WorldEnv
+from gym_platform.envs.clock import Clock
 
 
 register(
