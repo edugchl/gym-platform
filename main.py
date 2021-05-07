bee9dd85f3c468c2ca0f80b5207086a847acdb8d
garychl <garychl320@gmail.com>
2021-05-07 11:37:19 +0800

    for remote branch

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+TODO:
+    - Catching KeyboardInterrupt
+    - rebuild 
+"""
+
 import argparse
 import time
 from dateutil.relativedelta import relativedelta
@@ -6,71 +12,51 @@ import numpy as np
 from stable_baselines3 import A2C
 import wandb
 
-from gym_platform import LearningPlatform, UserEnv, WorldEnv
+from gym_platform import LearningPlatform, User, Clock
 from gym_platform.envs.utils import flatten_dict
 
 
 hyperparameter_defaults = {
     # general
+    'wandb': False, 
     'episodes': 100, 
     # environment
-    'world_time_ratio': 36000,
-    'user_job': 'WHITE COLLAR', 
-    'user_dependents': 0, 
-    'platform_doomsday': 20*24, 
+    'clock_ratio': 36000,
+    'job': 'WHITE COLLAR', 
+    'dependents': 0, 
+    'end_days': 365, 
+    'include_time_features': False, 
     # agent
-    'agent_timesteps': 20*24, 
+    'agent_timesteps': 100, 
 }
 
 
-def train(hyperparameters=None):
-    run = wandb.init(config=hyperparameters, project='rl-notification-system')
-    config = run.config
+def train(params=None):
 
-    world_env = WorldEnv(ratio=config.world_time_ratio)
-    user_env = UserEnv(world_env, config.user_job, config.user_dependents)
-    platform = LearningPlatform(user_env, world_env, config.platform_doomsday)
-
-    model = A2C('MlpPolicy', platform, verbose=1)
-    model.learn(total_timesteps=config.agent_timesteps)
-
+    clock = Clock(params['clock_ratio'])
+    start = clock.real_start
+    user = User(params['job'], params['dependents'], clock.artifical_start)
+    platform = LearningPlatform(user, clock, params['end_days'], params['include_time_features'])
     obs = platform.reset()
+    terminal = False
+    print(f'initial obs: {obs}')
+    # model = A2C('MlpPolicy', platform, verbose=1)
+    # model.learn(total_timesteps=params['agent_timesteps'])
 
-    for eps in range(config.episodes):
-        trajectory, states = [], []
-        cumulative_rewards, step = 0, 0
-        terminal = False
+    for eps in range(params['episodes']):
+        
         while not terminal:
-            time.sleep(3600/config.world_time_ratio)
-            # action = np.random.choice(2)
-            action, _state = model.predict(obs, deterministic=True)
-            obs, reward, terminal, _ = platform.step(action)
-            step += 1; cumulative_rewards += reward; 
-            trajectory.append([eps, step] + list(obs) + [action, reward])
-            states.append([eps, step] + list(flatten_dict(user_env.state).values()))
-            
-            print(f'state: {platform.user_env.state}\n, obs: {obs}\n, action:{action}, reward:{reward} \n')
-            
+            action = 0
+            # action, _state = model.predict(obs, deterministic=True)
+            obs, reward, terminal, _ = platform.step(action, )
+
+            # TODO: remove
+            # simulate time passes
+            # time.sleep(3600/params['clock_ratio'])
+
             if terminal:
                 obs = platform.reset()
-                run.log({
-                    'cumulative_rewards': cumulative_rewards, 
-                    'mean_rewards': cumulative_rewards/step})
-                
-                trajectory_table = wandb.Table(data=trajectory, columns=['episode', 'step']+platform.observation_name+['action', 'reward'])
-                states_table = wandb.Table(data=states, columns=['episode', 'step']+list(flatten_dict(user_env.state).keys()))
-                line_plot = wandb.plot.line(trajectory_table, x='step', y='reward', title='Line Plot')
-                histogram = wandb.plot.histogram(trajectory_table, value='reward', title='Histogram')
-                scatter = wandb.plot.scatter(trajectory_table, x='step', y='reward', title='Scatter Plot')
-
-                run.log({
-                    'line_1': line_plot, 
-                    'histogram_1': histogram, 
-                    'scatter_1': scatter,
-                    'observation_table': trajectory_table, 
-                    'state_table': states_table, 
-                    })
 
 
 if __name__ == '__main__':
-    train(hyperparameters=hyperparameter_defaults)
+    train(params=hyperparameter_defaults)
